d129efdbf986ed33ee5448bcfd9ebe6fc568d03c
NOJIMA Yusuke <nojima@kmc.gr.jp>
2012-06-08 22:28:31 +0900

    Add comments

--- lib/base64url.rb
+++ lib/base64url.rb
@@ -1,4 +1,26 @@
-# The Base64URL module provides for the encoding and decoding method using base64url representation.
+#
+# = base64url.rb: methods for base64url-encoding and -decoding strings
+#
+
+# The Base64URL module provides for the encoding and decoding of binary data
+# using a base64url representation.
+#
+# base64url encoding is Base64 for URLs and filenames.
+# base64url's 62nd and 63rd chars are '-' and '_'.
+# base64url does not have pad chars '='.
+#
+# == Example
+#
+# A simple encoding and decoding.
+#
+#     require "base64url"
+#
+#     enc   = Base64URL.encode('Send reinforcements')
+#                         # -> "U2VuZCByZWluZm9yY2VtZW50cw"
+#     plain = Base64URL.decode(enc)
+#                         # -> "Send reinforcements"
+#
+
 module Base64URL
   module_function
 
